25665941cdc41607a14aadf63304fa22c887c2e8
Derek Hubbard <derek.e.hubbard@disney.com>
2020-07-11 09:34:40 -0700

    Exercises 2.09 and 2.10

--- chapter02/2.09.cc
+++ chapter02/2.09.cc
@@ -2,23 +2,22 @@
 
 int main() {
     // a.
-    std::cin >> int input_value; // error, you cannot initialize and assign an object directly from standard input
-    
+    // std::cin >> int input_value; // error, you cannot initialize and assign an object directly from standard input
     int input_value;
     std::cin >> input_value;
 
-    
     // b.
-    int i = { 3.14 }; // error, double cannot be narrowed to an int in initializer list
-
+    // int i = { 3.14 }; // error, double cannot be narrowed to an int in initializer list
     double i = { 3.14 };
    
     // c.
-    double salary = wage = 9999.99; // error, use of undeclared identifier wage
-
+    // double salary = wage = 9999.99; // error, use of undeclared identifier wage
     double salary, wage;
     salary = wage = 9999.99;
 
     // d.
-    int i = 3.14; // legal, but value in `i` will be truncated to `3`.
+    // int i2 = 3.14; // legal, but value in `i` will be truncated to `3`.
+    double i2 = 3.14;
+
+    return 0;
 }
---
+++ chapter02/2.10.cc
@@ -0,0 +1,11 @@
+#include <string>
+
+std::string global_str; // default value `empty`
+int global_int; // default value `0`
+
+int main() {
+    int local_int; // default value `undefined`
+    std::string local_str; // default value `empty`
+
+    return 0;
+}
