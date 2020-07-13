c6bb14ff1c69a15aec63bd2aaacbed2e6a1be124
Derek Hubbard <derek.e.hubbard@disney.com>
2020-07-12 18:05:50 -0700

    Adds Exercise 2.12

---
+++ chapter02/2.12.cc
@@ -0,0 +1,18 @@
+int main() {
+    // a.
+    // int double = 3.14; // error, cannot combine types `int` and `double`
+
+    // b.
+    int _; // valid
+
+    // c.
+    // int catch-22; // error, cannot use hyphen
+
+    // d.
+    // int 1_or_2 = 1; // error, cannot start variable name with a number
+
+    // e.
+    double Double = 3.14; // valid
+
+    return 0;
+}
