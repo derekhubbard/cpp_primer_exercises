c002d7055ee6b2308e20e5fd870f56b60509b93a
Derek Hubbard <derek.e.hubbard@disney.com>
2020-08-03 17:26:03 -0700

    Const Exercises

--- chapter02/2.28.cc
+++ chapter02/2.28.cc
@@ -1,29 +1,19 @@
 int main() {
 
-    {
-        // a.
-        // int i, *const ip; // illegal, default initialization of a const object
-    }
-     
-    {
-        // b.
-        // int *p1, *const p2; // illegal, default initialization of a const object
-    }
+    // a.
+    // int i, *const ip; // illegal, default initialization of a const object
 
-    {
-        // c.
-        // const int ic, &r = ic; // illegal, default initialization of a const object
-    }
+    // b.
+    // int *p1, *const p2; // illegal, default initialization of a const object
 
-    {
-        // d.
-        // const int *const p3; // illegal, default initialization of a const object
-    }
+    // c.
+    // const int ic, &r = ic; // illegal, default initialization of a const object
 
-    {
-        // e.
-        const int *p; // ok
-    }
+    // d.
+    // const int *const p3; // illegal, default initialization of a const object
+
+    // e.
+    const int *p; // ok
 
     return 0;
 }
--- chapter02/2.29.cc
+++ chapter02/2.29.cc
@@ -1,29 +1,27 @@
 int main() {
+    int i, *const ip = &i;
+    int *p1, *const p2 = p1;
+    const int ic = 5, &r = ic;
+    const int *const p3 = &ic;
+    const int *p;
 
-    {
-        // a.
-        // int i, *const ip; // illegal, default initialization of a const object
-    }
-     
-    {
-        // b.
-        // int *p1, *const p2; // illegal, default initialization of a const object
-    }
+    // a.
+    i = ic; // ok
+ 
+    // b.
+    // p1 = p3; // illegal, cannot assign const int *const to int *
 
-    {
-        // c.
-        // const int ic, &r = ic; // illegal, default initialization of a const object
-    }
+    // c.
+    // p1 = &ic; // illegal, cannot assign  const int * to int *
 
-    {
-        // d.
-        // const int *const p3; // illegal, default initialization of a const object
-    }
+    // d.
+    // p3 = &ic; // illegal, cannot assign to variable with const type
 
-    {
-        // e.
-        const int *p; // ok
-    }
+    // e.
+    // p2 = p1; // illegal, cannot assign to variable with const type
+
+    // f.
+    // ic = *p3; // illegal, cannot assign to variable with const type
 
     return 0;
 }
